3da398b9f3f1af0f3e6e5858d9f192925579e8ba
jeremyphilemon <git@jrmyphlmn.com>
2024-04-09 15:25:25 +0300

    Fix icon errors

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -510,14 +510,14 @@ export const ArrowUpRight = () => {
     <svg
       data-testid="geist-icon"
       height="12"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
       viewBox="0 0 16 16"
       width="12"
       style={{ color: 'currentcolor' }}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M5.75001 2H5.00001V3.5H5.75001H11.4393L2.21968 12.7197L1.68935 13.25L2.75001 14.3107L3.28034 13.7803L12.4988 4.56182V10.25V11H13.9988V10.25V3C13.9988 2.44772 13.5511 2 12.9988 2H5.75001Z"
         fill="currentColor"
       ></path>
