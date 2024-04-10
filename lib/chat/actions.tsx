4f339379c9aa41a3f43a57f8f12908d0208fd010
jeremyphilemon <git@jrmyphlmn.com>
2024-04-10 16:12:44 +0300

    Close spinner stream before streaming ui

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -274,6 +274,7 @@ async function submitUserMessage(content: string) {
         if (toolName === 'listDestinations') {
           const { destinations } = args
 
+          spinnerStream.done(null)
           uiStream.done(
             <BotCard>
               <Destinations destinations={destinations} />
@@ -307,6 +308,7 @@ async function submitUserMessage(content: string) {
             ]
           })
 
+          spinnerStream.done(null)
           uiStream.done(
             <BotCard>
               <ListFlights summary={args} />
@@ -327,6 +329,7 @@ async function submitUserMessage(content: string) {
             ]
           })
 
+          spinnerStream.done(null)
           uiStream.done(
             <BotCard>
               <SelectSeats summary={args} />
@@ -347,6 +350,7 @@ async function submitUserMessage(content: string) {
             ]
           })
 
+          spinnerStream.done(null)
           uiStream.done(
             <BotCard>
               <ListHotels />
@@ -358,6 +362,7 @@ async function submitUserMessage(content: string) {
             interactions: []
           })
 
+          spinnerStream.done(null)
           uiStream.done(
             <BotCard>
               <PurchaseTickets />
@@ -378,6 +383,7 @@ async function submitUserMessage(content: string) {
             ]
           })
 
+          spinnerStream.done(null)
           uiStream.done(
             <BotCard>
               <BoardingPass summary={args} />
@@ -399,6 +405,7 @@ async function submitUserMessage(content: string) {
             ]
           })
 
+          spinnerStream.done(null)
           uiStream.done(
             <BotCard>
               <FlightStatus summary={args} />
@@ -410,7 +417,6 @@ async function submitUserMessage(content: string) {
 
     textStream.done()
     messageStream.done()
-    spinnerStream.done(null)
   })()
 
   return {
