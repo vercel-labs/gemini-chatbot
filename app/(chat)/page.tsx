d0818c956df13c5823431bf888447c0e88b4c45c
jeremyphilemon <git@jrmyphlmn.com>
2024-04-10 19:49:29 +0300

    Update metadata for new chat

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -6,7 +6,7 @@ import { Session } from '@/lib/types'
 import { getMissingKeys } from '../actions'
 
 export const metadata = {
-  title: 'Next.js AI Chatbot'
+  title: 'New Chat'
 }
 
 export default async function IndexPage() {
