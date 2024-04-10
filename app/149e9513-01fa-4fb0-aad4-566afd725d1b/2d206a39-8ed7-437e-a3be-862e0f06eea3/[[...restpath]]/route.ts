cd0e980bac2ce912d4df990e160dedcca3dd5f97
jeremyphilemon <git@jrmyphlmn.com>
2024-04-10 20:32:52 +0300

    Add kasada

---
+++ app/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/[[...restpath]]/route.ts
@@ -0,0 +1,37 @@
+export const runtime = 'edge'
+export const dynamic = 'force-dynamic'
+export const maxDuration = 3
+
+async function handler(request: Request) {
+  const url = new URL(request.url)
+
+  url.protocol = 'https:'
+  url.host = process.env.KASADA_API_ENDPOINT || ''
+  url.port = ''
+  url.searchParams.delete('restpath')
+
+  const headers = new Headers(request.headers)
+  headers.set('X-Forwarded-Host', process.env.KASADA_HEADER_HOST || '')
+  headers.delete('host')
+  const r = await fetch(url.toString(), {
+    method: request.method,
+    body: request.body,
+    headers,
+    mode: request.mode,
+    redirect: 'manual',
+    // @ts-expect-error
+    duplex: 'half'
+  })
+  const responseHeaders = new Headers(r.headers)
+  responseHeaders.set('cdn-cache-control', 'no-cache')
+  return new Response(r.body, {
+    status: r.status,
+    statusText: r.statusText,
+    headers: responseHeaders
+  })
+}
+
+export const GET = handler
+export const POST = handler
+export const OPTIONS = handler
+export const PUT = handler
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,29 @@
-import NextAuth from 'next-auth'
-import { authConfig } from './auth.config'
+import { kv } from '@vercel/kv'
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import { kasadaHandler } from './lib/kasada/kasada-server'
 
-export default NextAuth(authConfig).auth
+const MAX_REQUESTS = 50
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (req.method === 'POST') {
+    const realIp = req.headers.get('x-real-ip') || 'no-ip'
+    const pipeline = kv.pipeline()
+    pipeline.incr(`rate-limit:${realIp}`)
+    pipeline.expire(`rate-limit:${realIp}`, 60 * 60 * 24)
+    const [requests] = (await pipeline.exec()) as [number]
+
+    if (process.env.NODE_ENV === 'development') {
+      return undefined
+    }
+
+    if (requests > MAX_REQUESTS) {
+      return new Response('Too many requests', { status: 429 })
+    }
+
+    return kasadaHandler(req, ev)
+  }
+}
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
+  matcher: ['/', '/chat/:id*', '/share/:id*']
 }
