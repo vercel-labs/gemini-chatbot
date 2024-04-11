c64601e1802baef37898c1fe4b4e9aa9364a20a0
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-04-12 05:04:32 +0530

    follow typography rules
    Co-Authored-By: ruru <ruru.dev07@gmail.com>

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link'
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function NotFound() {
   return (
     <div className="flex space-x-2 w-full fixed inset-0 bg-card  justify-center items-center">
       <div className=" grid place-items-center">
-        <h1 className="text-9xl"> 404 </h1>
-        <h1 className="text-3xl my-5"> Page not found </h1>
-        <Link href={"/"}>
-          <Button variant={"outline"}>Back to home</Button>
+        <h1 className="text-9xl scroll-m-20 font-extrabold tracking-tight">
+          {' '}
+          404{' '}
+        </h1>
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight my-5">
+          Page not found
+        </h4>
+        <Link href={'/'}>
+          <Button>Back to home</Button>
         </Link>
       </div>
     </div>
