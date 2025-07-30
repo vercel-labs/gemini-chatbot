6bbfc0f514ffd843b04dd56254940b4d7fbcfea6
Daslin Davidson <daslin@appexert.com>
2025-07-30 18:44:07 +0530

    feat(multimodal-input): add user greeting messages and session handling; refactor input layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { Toaster } from "sonner";
+import { SessionProvider } from "next-auth/react";
 
 import { Navbar } from "@/components/custom/navbar";
 import { ThemeProvider } from "@/components/custom/theme-provider";
@@ -26,9 +27,11 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Toaster position="top-center" />
-          <Navbar />
-          {children}
+          <SessionProvider>
+            <Toaster position="top-center" />
+            <Navbar />
+            {children}
+          </SessionProvider>
         </ThemeProvider>
       </body>
     </html>
