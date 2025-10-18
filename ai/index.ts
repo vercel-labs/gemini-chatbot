1ee2a04a9a4cdd151aced449d237eb36c2eec96a
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2025-10-17 19:27:54 -0700

    update

--- ai/index.ts
+++ ai/index.ts
@@ -4,11 +4,11 @@ import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
 export const geminiProModel = wrapLanguageModel({
-  model: google("gemini-1.5-pro-002"),
+  model: google("gemini-2.5-pro"),
   middleware: customMiddleware,
 });
 
 export const geminiFlashModel = wrapLanguageModel({
-  model: google("gemini-1.5-flash-002"),
+  model: google("gemini-2.5-flash"),
   middleware: customMiddleware,
 });
